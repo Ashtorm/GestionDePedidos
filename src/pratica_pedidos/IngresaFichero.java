8ddd682bf83c7dd12e934d069099057b8fcf5fef
Ashtorm <alejandroevil9@gmail.com>
2023-04-30 20:11:53 +0200

    version 0.1
    Version estable y funcional con el añadido de las escritura de clientes

--- src/pratica_pedidos/IngresaFichero.java
+++ src/pratica_pedidos/IngresaFichero.java
@@ -3,6 +3,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 public class IngresaFichero {
+	//Funcionalidad de escritura de clientes en txt
 	public static void LectorFichero(Cliente Cliente1, Cliente Cliente2, Cliente Cliente3) throws IOException {
 	FileOutputStream fos=new FileOutputStream("C:/Users/aleja/Desktop/Escritura_Eclipse.txt");
 	ObjectOutputStream oos=new ObjectOutputStream(fos);
